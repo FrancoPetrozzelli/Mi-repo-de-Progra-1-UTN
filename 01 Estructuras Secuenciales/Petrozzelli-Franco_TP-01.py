50014e29ee3ccfb9a97d978e7a7fc50584d5a242
Petro <frankitoenzo@gmail.com>
2025-04-19 12:44:23 -0300

    Subida de primer tp

---
+++ 01 Estructuras Secuenciales/Petrozzelli-Franco_TP-01.py
@@ -0,0 +1,74 @@
+# Ejercicio 1
+
+# print("Hola mundo!")
+
+# Ejercicio 2
+
+# nombre = input("¿Cuál es tu nombre? ")
+# print(f"Hola {nombre}!")
+
+# Ejercicio 3
+
+# nombre = input("¿Cuál es tu nombre? ")
+# apellido = input("¿Cuál es tu apellido? ")
+# edad = input("¿Cuál es tu edad? ")
+# LugarResidencia = input("¿Cuál es tu lugar de residencia? ")
+# print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {LugarResidencia}.")
+
+# Ejercicio 4
+
+# radio = float(input("Ingrese el radio del círculo: "))
+# area = 3.14159 * radio**2
+# perimetro = 2 * 3.14159 * radio
+# print(f"El área del círculo es: {area}")
+# print(f"El perímetro del círculo es: {perimetro}")
+
+# Ejercicio 5
+
+# segundos = int(input("Ingrese la cantidad de segundos: "))
+# minutos = segundos // 60
+# horas = minutos // 60
+# print(f"{segundos} segundos son {horas} horas.")
+
+# Ejercicio 6
+
+# numero = int(input("Ingrese un número: "))
+# print(numero*1, numero*2, numero*3, numero*4, numero*5, numero*6, numero*7, numero*8, numero*9, numero*10)
+
+# Ejercicio 7
+
+# numero1 = int(input("Ingrese el primer número: "))
+# numero2 = int(input("Ingrese el segundo número: "))
+# if numero1 == 0 or numero2 == 0:
+#     print("Los numeros tienen que ser distintos de cero")
+# else:
+#     suma = numero1 + numero2
+#     resta = numero1 - numero2
+#     multiplicacion = numero1 * numero2
+#     division = numero1 / numero2
+#     print(f"La suma es: {suma}")
+#     print(f"La resta es: {resta}")
+#     print(f"La multiplicación es: {multiplicacion}")
+#     print(f"La división es: {division}")
+
+# Ejercicio 8
+
+# altura = float(input("Ingrese su altura: "))
+# peso =  float(input("Ingrese su peso en kg: "))
+# imc = peso / (altura**2)
+# print(f"Su IMC es: {imc}")
+
+# Ejercicio 9
+
+# TemperaturaCelcius = float(input("Ingrese la temperatura en Celsius: "))
+# TemperaturaFahrenheit = (TemperaturaCelcius * 9/5) + 32
+# print(f"La temperatura en Fahrenheit es: {TemperaturaFahrenheit}")
+
+
+# Ejercicio 10
+
+# numero1 = int(input("Ingrese el primer número: "))
+# numero2 = int(input("Ingrese el segundo número: "))
+# numero3 = int(input("Ingrese el tercer número: "))
+# promedio = (numero1 + numero2 + numero3) / 3
+# print(f"El promedio es: {promedio}")
